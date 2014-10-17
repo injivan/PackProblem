dc5896a06b5f657ddefa94a7b569aba47698867f
XPMUser <XPMUser@VPC-SFA-11>
2014-10-17 17:19:44 +0300

    a

--- packProblem/src/packProblem/myBoxxRow.java
+++ packProblem/src/packProblem/myBoxxRow.java
@@ -12,10 +12,32 @@ public class myBoxxRow {
 		startPoint.setD(d);
 		myPoints.add(startPoint);
 	}
-	public void addPack(pack pack1){
-		//find a free pleases to put the pack
+
+	public void addPack(pack pack1) {
+		// find a free pleases to put the pack
+		boolean flIn = true;
 		for (int i = 0; i < myPoints.size(); i++) {
-			
+			pack pointx = myPoints.get(i);
+			if (canAddPack(pointx, pack1)){
+				//have to add a new pack and make new addPints
+			}
+			 
 		}
+	}
+
+	private boolean canAddPack(pack point1, pack addetPack) {
+		boolean flIn = false;
+		int x, y, z;
+		x = point1.getX() + point1.getW() + addetPack.getX();
+		y = point1.getY() + point1.getH() + addetPack.getY();
+		z = point1.getZ() + point1.getD() + addetPack.getZ();
+		if (x <= startPoint.getW()) {
+			if (y <= startPoint.getH()) {
+				if (z <= startPoint.getD()) {
+					flIn = true;
+				}
+			}
+		}
+		return flIn;
 	}
 }
